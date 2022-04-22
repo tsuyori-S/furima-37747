a54b88031f2a18dcbc7e362892835bb7c8d2db62
tsuyori-S <tsuyosusuki@gmail.com>
2022-04-22 12:35:00 +0900

    単体テストコードの実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              {Faker::Name.name}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {"鈴木"}
+    first_name            {"一郎"}
+    ruby_last_name        {"スズキ"}
+    ruby_first_name       {"イチロウ"}
+    birth_date            {"1930-01-01"}
   end
 end
