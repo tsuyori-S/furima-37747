612b35c5473f797650e831eaaf0064143b860341
tsuyori-S <tsuyosusuki@gmail.com>
2022-04-27 23:10:04 +0900

    editアクションの追記とupdateアクションの定義
    editアクションを追記し、出品ユーザーとログインユーザーが異なる場合は編集ページにアクセスできないように設定した。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,10 +24,18 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def update
-
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   private
