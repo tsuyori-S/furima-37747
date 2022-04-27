46dcbced52277221b49ec8954f5d467e53f0a27d
tsuyori-S <tsuyosusuki@gmail.com>
2022-04-27 21:45:33 +0900

    rubocop実行後

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,7 +17,8 @@ class Item < ApplicationRecord
   validates :image,       presence: true
   validates :name,        presence: true
   validates :information, presence: true
-  validates :price,       presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price,       presence: true,
+                          numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   validates :category_id,      numericality: { other_than: 1, message: "can't be blank" }
   validates :state_id,         numericality: { other_than: 1, message: "can't be blank" }
