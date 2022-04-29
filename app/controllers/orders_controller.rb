896d04d79917b98f0662b36eec19a0cdd9f60fc7
tsuyori-S <tsuyosusuki@gmail.com>
2022-04-29 17:14:10 +0900

    ordersコントローラー　アクションの定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,23 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :province_id, :city, :house_number, :building, :phone_number).merge(order_id: order.id)
+  end
 end
